f0be3e215e33e986296f39671111c0a7f43e70f8
kaizju <rookieslit@gmail.com>
2026-04-20 14:53:19 +0800

    added some items on the feed

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,7 +1,16 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle } from "@ionic/react";
+import { IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonList, IonMenuButton, IonPage, IonTitle } from "@ionic/react";
 
 
 const Feed: React.FC = () => {
+
+    const games = [
+        {name: 'Pokemon Yellow'},
+        {name: 'Mega Man X'},
+        {name: 'The Legend of Zelda'},
+        {name: 'Pac-Man'},
+        {name: 'Super Mario World'},
+        {name: 'BomberMan'}
+    ]
   return (
     <IonPage>
         <IonHeader>
@@ -11,7 +20,14 @@ const Feed: React.FC = () => {
             </IonButtons>
         </IonHeader>
         <IonContent className="ion-padding">
-            <h1>Feed</h1>
+            <IonList>
+            {games.map((item,index)=>
+            <IonItem key={index}>
+<IonLabel>{item.name}</IonLabel>
+            </IonItem>
+            )}
+            </IonList>
+           
         </IonContent>
     </IonPage>
   )
